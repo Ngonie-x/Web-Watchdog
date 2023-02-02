2e59da62b25b1d392ebcbfbc774b614a1d8cb562
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2023-02-02 16:26:43 +0200

    Setup urls for documentation and authentication

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,8 +14,49 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from rest_framework_simplejwt import views as jwt_views
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Pay genius API",
+        default_version="v1",
+        description="An api to generate salary surveys",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="ngonidzashe@ipcconsultants.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('monitors/', include('monitors.urls')),
+
+    # Authentication
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
+
+    # Documentation
+    path(
+        'swagger/',
+        schema_view.with_ui(
+            'swagger', cache_timeout=0
+        ),
+        name='schema-swagger-ui'
+    ),
+
+    path(
+        'redoc/',
+        schema_view.with_ui(
+            'redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
 ]
