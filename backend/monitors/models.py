4dcd01e798df82d8c28a215623badac9741be7fa
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2023-02-02 16:20:33 +0200

    Added monitor model

--- backend/monitors/models.py
+++ backend/monitors/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Monitor(models.Model):
+    monitor_type = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100)
+    url_or_ip = models.CharField(max_length=100)
+    monitoring_interval = models.IntegerField()
+    monitor_ssl = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.friendly_name
