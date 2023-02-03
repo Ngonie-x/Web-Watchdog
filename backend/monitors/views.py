8437b0ecddb04639e4febe305b4a292f3e4927dc
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2023-02-03 16:19:22 +0200

    added queryset

--- backend/monitors/views.py
+++ backend/monitors/views.py
@@ -2,10 +2,14 @@ from rest_framework import generics
 
 from .serializers import MonitorSerializer
 
+from .models import Monitor
+
 
 class MonitorListView(generics.ListCreateAPIView):
     serializer_class = MonitorSerializer
+    queryset = Monitor.objects.all()
 
 
 class MonitorView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MonitorSerializer
+    queryset = Monitor.objects.all()
