be4dbbc2f301db22b8ac8a42a3f3d421df5ec7ce
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2023-02-06 16:21:52 +0200

    Added monitor status model

--- backend/monitors/models.py
+++ backend/monitors/models.py
@@ -7,6 +7,17 @@ class Monitor(models.Model):
     url_or_ip = models.CharField(max_length=100)
     monitoring_interval = models.IntegerField()
     monitor_ssl = models.BooleanField(default=False)
+    port = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.friendly_name
+
+
+class MonitorStatus(models.Model):
+    monitor = models.ForeignKey(Monitor, on_delete=models.CASCADE)
+    url_ip_up = models.BooleanField(default=False)
+    ssl_up = models.BooleanField(default=False)
+    date_timer = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.monitor.friendly_name
