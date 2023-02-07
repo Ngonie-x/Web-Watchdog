10ab945f3435a140e6e68d681a923d861c863f22
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2023-02-07 21:44:22 +0200

    Registered to admin

--- backend/monitors/admin.py
+++ backend/monitors/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Monitor, MonitorStatus
+
+
+@admin.register(Monitor)
+class MonitorAdmin(admin.ModelAdmin):
+    list_display = (
+        'monitor_type',
+        'friendly_name',
+        'url_or_ip',
+        'monitoring_interval',
+        'monitor_ssl',
+        'port'
+    )
+
+    list_filter = (
+        'friendly_name',
+        'url_or_ip'
+    )
+
+
+@admin.register(MonitorStatus)
+class MonitorStatusAdmin(admin.ModelAdmin):
+    list_display = (
+        'monitor',
+        'url_ip_up',
+        'ssl_up',
+        'date_timer'
+    )
+
+    list_filter = (
+        'monitor',
+    )
