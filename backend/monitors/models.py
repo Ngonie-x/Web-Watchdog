f30b1450c06d6878d12081d29dc11dd7a0998bf4
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2023-02-07 21:58:53 +0200

    Added user and choices

--- backend/monitors/models.py
+++ backend/monitors/models.py
@@ -1,12 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+monitor_choices = [
+    ['Ping', 'Ping'],
+    ['HTTP(S)', 'HTTP(S)'],
+    ['Port', 'Port']
+]
 
 
 class Monitor(models.Model):
-    monitor_type = models.CharField(max_length=100)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    monitor_type = models.CharField(max_length=100, choices=monitor_choices)
+
     friendly_name = models.CharField(max_length=100)
+
     url_or_ip = models.CharField(max_length=100)
+
     monitoring_interval = models.IntegerField()
+
+    monitor_timeout = models.IntegerField(default=30)
+
     monitor_ssl = models.BooleanField(default=False)
+
     port = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
@@ -14,9 +31,13 @@ class Monitor(models.Model):
 
 
 class MonitorStatus(models.Model):
+
     monitor = models.ForeignKey(Monitor, on_delete=models.CASCADE)
+
     url_ip_up = models.BooleanField(default=False)
+
     ssl_up = models.BooleanField(default=False)
+
     date_timer = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
